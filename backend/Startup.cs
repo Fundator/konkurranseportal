84d0a74458ead47f83ba0bccfd473522a482487b
Elias Sundby Aukan <esundbya@gmail.com>
2019-11-21 20:13:45 +0100

    fix cors

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -12,6 +12,7 @@ namespace TodoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Context>(opt => opt.UseInMemoryDatabase("ELISD.db"));
+            services.AddCors();
             services.AddMvc();
 
             // Register the Swagger generator, defining one or more Swagger documents
@@ -31,6 +32,9 @@ namespace TodoApi
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:3000").AllowAnyMethod();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
